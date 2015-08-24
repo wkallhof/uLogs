869fed778be444943cc1d47eace3f01445539b55
Wade Kallhoff <wade.kallhoff@gmail.com>
2015-08-23 19:25:15 -0500

    Updating with package info for NuGet

--- source/uLogs/Properties/AssemblyInfo.cs
+++ source/uLogs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wmk.LogViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("uLogs")]
+[assembly: AssemblyDescription("Umbraco Trace Log viewer for the Umbraco Back-Office")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wmk.LogViewer")]
+[assembly: AssemblyCompany("WMK.IO")]
+[assembly: AssemblyProduct("uLogs")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
